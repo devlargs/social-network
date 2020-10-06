3f765dc92ad1627eb6d8d122e3dd0196184d2728
Ralph Largo <devlargs@gmail.com>
2020-10-06 19:40:08 +0800

    chore: change background image for home

--- constants/links.ts
+++ constants/links.ts
@@ -1,4 +1,5 @@
+const s3Link = "https://socialnetworkrl.s3-ap-southeast-1.amazonaws.com/images";
+
 export default {
-  LOGO:
-    "https://socialnetworkrl.s3-ap-southeast-1.amazonaws.com/images/logo.png",
+  LOGO: `${s3Link}/logo.png`,
 };
