9b9df82f8728576f85fcc569ca57c7db0ce567b8
Ralph Largo <devlargs@gmail.com>
2020-10-09 06:05:02 +0800

    feat: added content loader

---
+++ components/ContentLoader.tsx
@@ -0,0 +1,31 @@
+import ContentLoader from "react-content-loader";
+
+type Props = {
+  width?: number;
+  height?: number;
+  backgroundColor?: string;
+  foregroundColor?: string;
+};
+
+export default ({
+  width = 200,
+  height = 70,
+  backgroundColor = "#f7f2f2",
+  foregroundColor = "#CCCCCC",
+}: Props) => (
+  <ContentLoader
+    speed={1}
+    width={width}
+    height={height}
+    viewBox="0 0 200 70"
+    backgroundColor={backgroundColor}
+    foregroundColor={foregroundColor}
+  >
+    <rect x="48" y="8" rx="3" ry="3" width="88" height="6" />
+    <rect x="48" y="26" rx="3" ry="3" width="52" height="6" />
+    <rect x="0" y="56" rx="3" ry="3" width="410" height="6" />
+    <rect x="0" y="72" rx="3" ry="3" width="380" height="6" />
+    <rect x="0" y="88" rx="3" ry="3" width="178" height="6" />
+    <circle cx="20" cy="20" r="20" />
+  </ContentLoader>
+);
--- components/ProfileCard.tsx
+++ components/ProfileCard.tsx
@@ -1,9 +1,10 @@
 import { useQuery } from "@apollo/react-hooks";
 import { ACCOUNT } from "queries/accounts";
 import coercedGet from "utils/coercedGet";
+import ContentLoader from "./ContentLoader";
 
 const ProfileCard = () => {
-  const { data, loading, error } = useQuery(ACCOUNT, {
+  const { data, loading } = useQuery(ACCOUNT, {
     variables: {
       id: "ckfxrhyao0ppz0185gv6gvxrl",
     },
@@ -13,21 +14,27 @@ const ProfileCard = () => {
 
   return (
     <div className="profile-card">
-      <img
-        src={account?.avatar?.url}
-        width={300}
-        height={300}
-        alt="user"
-        className="profile-photo"
-      />
-      <h5>
-        <a href="timeline" className="text-white">
-          {account?.firstName} {account?.lastName}
-        </a>
-      </h5>
-      <a href="#" className="text-white">
-        <i className="ion ion-android-person-add"></i> 0 followers
-      </a>
+      {loading ? (
+        <ContentLoader />
+      ) : (
+        <>
+          <img
+            src={account?.avatar?.url}
+            width={300}
+            height={300}
+            alt="user"
+            className="profile-photo"
+          />
+          <h5>
+            <a href="timeline" className="text-white">
+              {account?.firstName} {account?.lastName}
+            </a>
+          </h5>
+          <a href="#" className="text-white">
+            <i className="ion ion-android-person-add"></i> 0 followers
+          </a>
+        </>
+      )}
     </div>
   );
 };
