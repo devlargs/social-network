ebacb2fd7ba21b6c2d1349e1e7052f3297f06c6d
Ralph Largo <devlargs@gmail.com>
2020-10-09 08:54:03 +0800

    chore: refactor large options

--- components/RegisterForm.tsx
+++ components/RegisterForm.tsx
@@ -1,5 +1,6 @@
 import CountriesSelect from "components/CountriesSelect";
 import Link from "next/link";
+import months from "constants/months";
 
 const RegisterForm = () => (
   <>
@@ -79,37 +80,9 @@ const RegisterForm = () => (
             <option value="Day" disabled selected>
               Day
             </option>
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
-            <option>4</option>
-            <option>5</option>
-            <option>6</option>
-            <option>7</option>
-            <option>8</option>
-            <option>9</option>
-            <option>10</option>
-            <option>11</option>
-            <option>12</option>
-            <option>13</option>
-            <option>14</option>
-            <option>15</option>
-            <option>16</option>
-            <option>17</option>
-            <option>18</option>
-            <option>19</option>
-            <option>20</option>
-            <option>21</option>
-            <option>22</option>
-            <option>23</option>
-            <option>24</option>
-            <option>25</option>
-            <option>26</option>
-            <option>27</option>
-            <option>28</option>
-            <option>29</option>
-            <option>30</option>
-            <option>31</option>
+            {Array.from(Array(31).keys()).map((q) => (
+              <option value={1 + q}>{1 + q}</option>
+            ))}
           </select>
         </div>
         <div className="form-group col-sm-3 col-xs-6">
@@ -118,18 +91,9 @@ const RegisterForm = () => (
             <option value="month" disabled selected>
               Month
             </option>
-            <option>Jan</option>
-            <option>Feb</option>
-            <option>Mar</option>
-            <option>Apr</option>
-            <option>May</option>
-            <option>Jun</option>
-            <option>Jul</option>
-            <option>Aug</option>
-            <option>Sep</option>
-            <option>Oct</option>
-            <option>Nov</option>
-            <option>Dec</option>
+            {months.map((q) => (
+              <option value={q}>{q}</option>
+            ))}
           </select>
         </div>
         <div className="form-group col-sm-6 col-xs-12">
@@ -138,18 +102,11 @@ const RegisterForm = () => (
             <option value="year" disabled selected>
               Year
             </option>
-            <option>2000</option>
-            <option>2001</option>
-            <option>2002</option>
-            <option>2004</option>
-            <option>2005</option>
-            <option>2006</option>
-            <option>2007</option>
-            <option>2008</option>
-            <option>2009</option>
-            <option>2010</option>
-            <option>2011</option>
-            <option>2012</option>
+            {Array.from(Array(71).keys())
+              .reverse()
+              .map((q) => (
+                <option value={1950 + q}>{1950 + q}</option>
+              ))}
           </select>
         </div>
       </div>
